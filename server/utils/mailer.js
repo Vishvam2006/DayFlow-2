c8916665ee905ce094fe19957dfd905aaebf020b
Vishvam2006 <vishvam.r.modi@gmail.com>
2026-03-31 23:29:05 +0530

    BREVO-SETUP-2

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,26 +1,28 @@
-import nodemailer from 'nodemailer'
+import nodemailer from "nodemailer";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
+  secure: false,
   auth: {
-    user: process.env.BREVO_SMPT_MAIL,
-    pass: process.env.BREVO_SMPT_PASS,
+    user: process.env.BREVO_SMTP_MAIL,  // ✅ fixed
+    pass: process.env.BREVO_SMTP_PASS,  // ✅ fixed
   },
 });
 
 export const sendOTP = async (email, otp) => {
   try {
     const info = await transporter.sendMail({
+      from: `"DayFlow" <${process.env.BREVO_SMTP_MAIL}>`, // IMPORTANT
       to: email,
       subject: "Login OTP",
       text: `Your OTP is ${otp}`,
     });
 
     console.log("Email sent:", info.response);
-
   } catch (error) {
     console.error("MAIL ERROR:", error);
   }
